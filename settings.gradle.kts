49854463eeb7ac1d6fffe6f7d4e05ee978755c77
Script1-afk <wellotprince@gmail.com>
2025-11-16 01:56:48 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,12 @@
 rootProject.name = "AliucordPlugins"
 
+// Inclure tes plugins
 include(
     "MyFirstCommand",
     "MyFirstPatch"
 )
 
+// Si tes plugins sont dans ExamplePlugins/kotlin
 rootProject.children.forEach {
     it.projectDir = file("ExamplePlugins/kotlin/${it.name}")
 }
