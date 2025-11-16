4c0899af24beefe84b86faa6a3e2286bdbf3b342
Script1-afk <wellotprince@gmail.com>
2025-11-16 02:06:51 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,42 @@
+// build.gradle.kts
+
 buildscript {
     repositories {
         google()
         mavenCentral()
-        maven("https://maven.aliucord.com/snapshots")
-        maven("https://jitpack.io")
+        maven("https://maven.aliucord.com/releases") // Repo Aliucord
     }
-
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.4")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
-        classpath("com.aliucord.gradle:gradle-plugin:main-SNAPSHOT")
+        classpath("com.aliucord.gradle:gradle-plugin:2.1.2") // version stable
     }
 }
 
 plugins {
-    kotlin("android") version "1.5.21" apply false
+    kotlin("jvm") version "1.5.21" apply false
 }
 
-repositories {
-    google()
-    mavenCentral()
-    maven("https://maven.aliucord.com/snapshots")
-    maven("https://jitpack.io")
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.aliucord.com/releases")
+    }
 }
-
-group = "com.yourusername.plugins"
-version = "1.0.0"
 
 apply(plugin = "com.android.library")
-apply(plugin = "org.jetbrains.kotlin.android")
-apply(plugin = "com.aliucord.gradle")
+apply(plugin = "kotlin-android")
+apply(plugin = "com.aliucord.gradle.plugin")
+
+aliucord {
+    author("DISCORD USERNAME", 123456789L)
+    updateUrl.set("https://raw.githubusercontent.com/USERNAME/REPONAME/builds/updater.json")
+    buildUrl.set("https://raw.githubusercontent.com/USERNAME/REPONAME/builds/%s.zip")
+}
 
 android {
-    compileSdk = 31
+    compileSdkVersion(31)
 
     defaultConfig {
         minSdk = 24
@@ -44,22 +48,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "11"
-        }
+    kotlinOptions {
+        jvmTarget = "11"
     }
-}
-
-aliucord {
-    author("TonPseudoDiscord", 123456789012345678)
-    updateUrl.set("https://raw.githubusercontent.com/USERNAME/REPONAME/builds/updater.json")
-    buildUrl.set("https://raw.githubusercontent.com/USERNAME/REPONAME/builds/%s.zip")
-}
-
-dependencies {
-    implementation("androidx.appcompat:appcompat:1.4.0")
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
 }
